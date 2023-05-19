1bcdd3dbc05bc1f06e2ab12368590d5a6b7fcaa6
kimseokhee <cat1181123@naver.com>
2023-05-19 11:49:37 +0900

    fix: delete recoil

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import { RecoilRoot } from "recoil";
 import GlobalStyles from "./styles/globalStyles";
 import { CookiesProvider } from "react-cookie";
 import { QueryClient, QueryClientProvider } from "react-query";
